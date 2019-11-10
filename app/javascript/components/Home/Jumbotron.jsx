ffc489f980eed8ec72da1af5118bb1518b620f16
karamalesa1980 <karamalesa@gmail.com>
2019-11-10 23:58:26 +0200

    yarn add styled-components

--- app/javascript/components/Home/Jumbotron.jsx
+++ app/javascript/components/Home/Jumbotron.jsx
@@ -7,7 +7,7 @@ const Jumbotron = () => {
         <div className="row">
           <div className="col col-sm-12 col-md-5">
             <div className="pt-4 mt-4">
-              <h1>React For Rails Developers</h1>
+              <h1>React For Rails Developers!</h1>
               <p>Supercharge your Ruby on Rails Apps with React.js</p>
             </div>
           </div>
