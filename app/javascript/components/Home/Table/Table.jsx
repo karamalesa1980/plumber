842da9cfdd8ac0a933d52cbae6bae4375bddf6e6
karamalesa1980 <karamalesa@gmail.com>
2019-11-15 19:18:46 +0200

    add Video component (42:22)

--- app/javascript/components/Home/Table/Table.jsx
+++ app/javascript/components/Home/Table/Table.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Item from './Item'
+import ActiveItem from './ActiveItem'
 
 class Table extends Component {
 	constructor(props){
@@ -8,7 +9,11 @@ class Table extends Component {
 
 	render(){
 		const items = this.props.course_modules.map((data) => {
-			return <Item key={data.id} title={data.title} description={data.description} />
+			return (
+				data.active ?
+				<ActiveItem key={data.id} title={data.title} description={data.description} /> :
+				<Item key={data.id} title={data.title} description={data.description} />
+			)
 		})
 
 		return(
---
+++ app/javascript/components/Home/Table/Video.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Video = () => {
+  return (
+    <div className="pt-4 pb-4">
+      <iframe id="ytplayer" type="text/html" width="100%" height="400"
+              src="http://www.youtube.com/embed/aM8jH8pmmO0"
+              frameBorder="0"/> 
+    </div>
+  )
+}
+
+export default Video
